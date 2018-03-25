e7037d984bc18136dce4dccb253dda982a88f736
David M <davidmh.mail@gmail.com>
2018-03-25 18:53:41 -0500

    Move book CRUD operations to bookController

--- controllers/bookController.js
+++ controllers/bookController.js
@@ -17,7 +17,7 @@ const bookController = (Book) => {
         }
       });
     }
-  }
+  };
 
   const get = (req, res) => {
     const query = {};
@@ -44,11 +44,68 @@ const bookController = (Book) => {
         res.json(returnBooks);
       }
     });
-  }
+  };
+
+  const getOne = (req, res) => {
+    // add hypermedia to returned object
+    const returnBook = req.book.toJSON();
+    const filterByGenreLink = 'http://' + req.headers.host + '/api/books?genre=' + returnBook.genre;
+    returnBook.links = {
+      FilterByGenre: filterByGenreLink.replace(' ', '%20')
+    };
+
+    res.json(returnBook);
+  };
+
+  const put = (req, res) => {
+    req.book.title = req.body.title;
+    req.book.genre = req.body.genre;
+    req.book.author = req.body.author;
+    req.book.read = req.body.read;
+
+    req.book.save((err) => {
+      if (err) {
+        res.status(500).send(err);
+      } else {
+        res.json(req.book);
+      }
+    });
+  };
+
+  const patch = (req, res) => {
+    if (req.body._id) {
+      delete req.body._id;
+    }
+    for (let prop in req.body) {
+      req.book[prop] = req.body[prop];
+    }
+
+    req.book.save((err) => {
+      if (err) {
+        res.status(500).send(err);
+      } else {
+        res.json(req.book);
+      }
+    });
+  };
+
+  const deleteBook = (req, res) => {
+    req.book.remove((err) => {
+      if (err) {
+        res.status(500).send(err);
+      } else {
+        res.status(204).send('Removed');
+      }
+    });
+  };
 
   return {
     post: post,
-    get: get
+    get: get,
+    getOne: getOne,
+    put: put,
+    patch: patch,
+    delete: deleteBook
   };
 };
 
--- routes/bookRoutes.js
+++ routes/bookRoutes.js
@@ -23,55 +23,10 @@ const routes = (Book) => {
   });
 
   bookRouter.route('/:bookId')
-    .get((req, res) => {
-      // add hypermedia to returned object
-      const returnBook = req.book.toJSON();
-      const filterByGenreLink = 'http://' + req.headers.host + '/api/books?genre=' + returnBook.genre;
-      returnBook.links = {
-        FilterByGenre: filterByGenreLink.replace(' ', '%20')
-      };
-
-      res.json(returnBook);
-    })
-    .put((req, res) => {
-      req.book.title = req.body.title;
-      req.book.genre = req.body.genre;
-      req.book.author = req.body.author;
-      req.book.read = req.body.read;
-
-      req.book.save((err) => {
-        if (err) {
-          res.status(500).send(err);
-        } else {
-          res.json(req.book);
-        }
-      });
-    })
-    .patch((req, res) => {
-      if (req.body._id) {
-        delete req.body._id;
-      }
-      for (let prop in req.body) {
-        req.book[prop] = req.body[prop];
-      }
-
-      req.book.save((err) => {
-        if (err) {
-          res.status(500).send(err);
-        } else {
-          res.json(req.book);
-        }
-      });
-    })
-    .delete((req, res) => {
-      req.book.remove((err) => {
-        if (err) {
-          res.status(500).send(err);
-        } else {
-          res.status(204).send('Removed');
-        }
-      });
-    });
+    .get(bookController.getOne)
+    .put(bookController.put)
+    .patch(bookController.patch)
+    .delete(bookController.delete);
 
   return bookRouter;
 };
