74ddc98e56d5b569c461044b524f219d95a41522
David M <davidmh.mail@gmail.com>
2018-03-25 02:08:27 -0500

    Add get book by id & post book

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 const express = require('express'),
-      mongoose = require('mongoose');
+      mongoose = require('mongoose'),
+      bodyParser = require('body-parser');
 
 const db = mongoose.connect('mongodb://localhost/bookAPI');
 
@@ -8,9 +9,21 @@ const Book = require('./models/book');
 const app = express();
 const port = process.env.PORT || 3000;
 
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(bodyParser.json());
+
 const bookRouter = express.Router();
 
 bookRouter.route('/Books')
+  .post((req, res) => {
+    const book = new Book(req.body);
+
+    book.save().then(() => {
+      res.status(201).send(book);
+    }).catch((err) => {
+      res.status(400).send(err);
+    });
+  })
   .get((req, res) => {
     const query = {};
 
@@ -24,6 +37,17 @@ bookRouter.route('/Books')
         res.status(500).send(err);
       } else {
         res.json(books);
+      }
+    });
+  });
+
+bookRouter.route('/Books/:bookId')
+  .get((req, res) => {
+    Book.findById(req.params.bookId, (err, book) => {
+      if (err) {
+        res.status(500).send(err);
+      } else {
+        res.json(book);
       }
     });
   });
