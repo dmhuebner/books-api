3002b54f00df6474d0d7ae3edf2565468c5a73d8
David M <davidmh.mail@gmail.com>
2018-03-25 17:06:31 -0500

    Implement patch & delete operations and add middleware for :bookId routes

--- routes/bookRoutes.js
+++ routes/bookRoutes.js
@@ -30,13 +30,60 @@ const routes = (Book) => {
       });
     });
 
+  // Middleware for :bookId routes
+  bookRouter.use('/:bookId', (req, res, next) => {
+    Book.findById(req.params.bookId, (err, book) => {
+      if (err) {
+        res.status(500).send(err);
+      } else if (book) {
+        req.book = book;
+        next();
+      } else {
+        res.status(404).send('No book found');
+      }
+    });
+  });
+
   bookRouter.route('/:bookId')
     .get((req, res) => {
-      Book.findById(req.params.bookId, (err, book) => {
+      res.json(req.book);
+    })
+    .put((req, res) => {
+      req.book.title = req.body.title;
+      req.book.genre = req.body.genre;
+      req.book.author = req.body.author;
+      req.book.read = req.body.read;
+
+      req.book.save((err) => {
         if (err) {
           res.status(500).send(err);
         } else {
-          res.json(book);
+          res.json(req.book);
+        }
+      });
+    })
+    .patch((req, res) => {
+      if (req.body._id) {
+        delete req.body._id;
+      }
+      for (let prop in req.body) {
+        req.book[prop] = req.body[prop];
+      }
+
+      req.book.save((err) => {
+        if (err) {
+          res.status(500).send(err);
+        } else {
+          res.json(req.book);
+        }
+      });
+    })
+    .delete((req, res) => {
+      req.book.remove((err) => {
+        if (err) {
+          res.status(500).send(err);
+        } else {
+          res.status(204).send('Removed');
         }
       });
     });
